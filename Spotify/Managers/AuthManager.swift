edfda999384621dfac5de4b6abc12c4a3668d6a5
turgunbaevaa <nyc.aruu@gmail.com>
2024-05-03 21:51:02 +0200

    Added authorization function, have some bugs

--- Spotify/Managers/AuthManager.swift
+++ Spotify/Managers/AuthManager.swift
@@ -10,7 +10,21 @@ import Foundation
 final class AuthManager {
     static let shared = AuthManager()
     
+    struct Constants {
+        static let clientID = "64c5ac588ccc4790b149c7c05225b7a0"
+        static let clientSecret = "168c9c3165c44ac78cdc69e685e7e335"
+        static let staticAPIURL = "https://accounts.spotify.com/api/token"
+    }
+    
     private init() {}
+    
+    public var signInURL: URL? {
+        let scopes = "user-read-private"
+        let redirectURI = "https://www.iosacademy.io"
+        let base = "https://accounts.spotify.com/authorize"
+        let string = "\(base)?response_type=code&client_id=\(Constants.clientID)&scope=\(scopes)&redirect_uri=\(redirectURI)&show_dialog=TRUE"
+        return URL(string: string)
+    }
     
     var isSignedIn: Bool {
         return false
@@ -30,5 +44,63 @@ final class AuthManager {
     
     private var shouldRefreshToken: Bool {
         return false
+    }
+    
+    public func exchangeCodeForToken(_ code: String, completion: @escaping ((Bool) -> Void)) {
+        //GET TOKEN
+        guard let url = URL(string: Constants.staticAPIURL) else {
+            return
+        }
+        
+        var components = URLComponents()
+        components.queryItems = [
+            URLQueryItem(name: "grant_type",
+                         value: "authorization_code"),
+            URLQueryItem(name: "code",
+                         value: code),
+            URLQueryItem(name: "redirect_uri",
+                         value: "https://www.iosacademy.io"),
+            
+        ]
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        request.setValue("application/x-www-form-urlencoded", forHTTPHeaderField: "Content-Type")
+        request.httpBody = components.query?.data(using: .utf8)
+        
+        let basicToken = Constants.clientID+"+"+Constants.clientSecret
+        let data = basicToken.data(using: .utf8)
+        guard let base64String = data?.base64EncodedString() else {
+            print("Failure to get base 64")
+            completion(false)
+            return
+        }
+        
+        request.setValue("Basic: \(base64String)", forHTTPHeaderField: "Authorization")
+        
+        let task = URLSession.shared.dataTask(with: request) { data, _, error in
+            guard let data = data, error == nil else {
+                completion(false)
+                return
+            }
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: .allowFragments)
+                print("SUCCESS: \(json)")
+                completion(true)
+            } catch {
+                print(error.localizedDescription)
+                completion(false)
+            }
+        }
+        task.resume()
+    }
+    
+    private func cacheToken() {
+        
+    }
+    
+    private func refreshAccessToken() {
+        
     }
 }
---
+++ Spotify/Resources/Extensions.swift
@@ -0,0 +1,34 @@
+//
+//  Extensions.swift
+//  Spotify
+//
+//  Created by Aruuke Turgunbaeva on 3/5/24.
+//
+
+import UIKit
+
+extension UIView {
+    var width: CGFloat {
+        return frame.size.width
+    }
+    
+    var height: CGFloat {
+        return frame.size.height
+    }
+    
+    var left: CGFloat {
+        return frame.origin.x
+    }
+    
+    var right: CGFloat {
+        return left + width
+    }
+    
+    var top: CGFloat {
+        return frame.origin.y
+    }
+    
+    var bottom: CGFloat {
+        return top + height
+    }
+}
